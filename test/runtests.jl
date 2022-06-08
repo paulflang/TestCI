6a7bd270f6c851c26a9ee73aa1c40650f18ec5b5
paulflang <paul.lang@wolfson.ox.ac.uk>
2022-06-08 23:03:47 +0100

    add logfile to write

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,8 @@ using Test
 logdir = joinpath(@__DIR__, "logs")
 ispath(logdir) && rm(logdir, recursive=true)
 mkdir(logdir)
+open("logfile.txt", "w") do f
+    write(f, "Hello World!")
+end
 
 @test false
